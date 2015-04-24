891cf152901a21b1efca8c062056dada9e042aea
Masato Ikeda <masato.ikeda@gmail.com>
2015-04-25 00:23:20 +0900

    Re-implement XML parsing with bucket.ParseXML()

---
+++ bucket/bucket.go
@@ -0,0 +1,54 @@
+package bucket
+
+import (
+	"encoding/xml"
+	"io"
+	"io/ioutil"
+)
+
+type ListBucketResult struct {
+	Name        string
+	Prefix      string
+	Marker      string
+	IsTruncated string
+	Contents    []Contents
+}
+
+type Contents struct {
+	Key            string
+	Generation     int
+	MetaGeneration int
+	LastModified   string
+	ETag           string
+	Size           int
+	Owner          string
+}
+
+func ParseXML(in io.Reader) (*ReleaseList, error) {
+	data, err := ioutil.ReadAll(in)
+	if err != nil {
+		return nil, err
+	}
+
+	result := ListBucketResult{}
+	err = xml.Unmarshal(data, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	list := NewReleaseList()
+	var release *Release
+	for _, c := range result.Contents {
+		release, err = NewRelease(c.Key)
+		if err != nil {
+			if err == ParseError {
+				continue
+			} else {
+				return nil, err
+			}
+		}
+		list.add(*release)
+	}
+
+	return list, nil
+}
